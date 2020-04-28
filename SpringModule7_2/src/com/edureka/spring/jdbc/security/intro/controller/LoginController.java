ffb50dc127aa821d6d33597a5105be8205e00849
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-04-28 09:55:25 +0530

    Add files via upload
    Edureka Spring Framework Module7 Assignment

---
+++ SpringModule7_2/src/com/edureka/spring/jdbc/security/intro/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.edureka.spring.jdbc.security.intro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();		
+		model.setViewName("landingPage");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Incorrect username and password!");
+		}	
+		model.setViewName("login");
+		return model;
+
+	}
+
+	
+}
